c13e043ee3c6ff18bdb61387e77af66461178a7b
James Morrison <S01223106@acad.tri-c.edu>
2020-10-29 13:20:37 -0400

    Lab 2 completed

---
+++ Lab02/Lab02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("{0}\n{1}", "Hello World!", "From Noah Morrison");
+            Console.ReadKey();
+        }
+    }
+    /* 1. The program starts at the main method. 
+     * 2. A floating-point variable has twice as more memory than a integer type file. A float has a decimal place, while an integer does not.
+     * 3. myAccount.Name = IT1050-NoahMorrision; Console.WhiteLine($"Initial name is: {myAccount.Name}");
+     * 4-1. To read the value of an instance variable, we create method called a getter accessor.
+     * 4-2. To assign a value ton instance variable, we use a method called a setter accessor.
+     * 5. A class is a template for objects. The amount of objects we make is only limited to the amount of data we have on our computers. 
+     */
+}

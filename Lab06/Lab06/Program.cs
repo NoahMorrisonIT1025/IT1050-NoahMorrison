2d19d1a3af604a05b6d477b0542b0fe4ba91f26d
James Morrison <S01223106@acad.tri-c.edu>
2020-11-27 12:39:24 -0500

    Lab06 completed

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -10,29 +10,78 @@ namespace Lab06
     {
         static void Main()
         {
-            //p[0], p[1], p[2], p[3]
-            {
-                string[] months = new string[12] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-                for (int i = 0; i < months.Length; i++)
-                {
-                    Console.WriteLine(months[i]);
-                }
-                Console.ReadLine();
-            }
-            {
-                string[] seasons = { "Spring", "Summer", "Fall", "Winter" };
-                foreach (string i in seasons)
-                {
-                    Console.WriteLine(i);
-                }
-                Console.ReadLine();
-            }
-            {
-                Random random = new Random();
-                int randomNumber
+            //2. p[0], p[1], p[2], p[3]
 
-                    randomNumber = random.Next(0, 100);
+            //3
+            string[] months = new string[12] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            for (int i = 0; i < months.Length; i++)
+            {
+                Console.WriteLine(months[i]);
             }
+            Console.ReadLine();
+
+            //4
+            string[] seasons = { "Spring", "Summer", "Fall", "Winter" };
+            foreach (string i in seasons)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+            //5
+            var arr = new int[1000];
+            Random random = new Random();
+            int randomNumber;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                randomNumber = random.Next(0, 100);
+                arr[i] = randomNumber;
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.ReadLine();
+
+            //6
+            string[] names = { "Al Dente", "Anna Ghrahm", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+            int j = 0;
+            while (j < names.Length)
+            {
+                Console.WriteLine(names[j]);
+                j++;
+            }
+            Console.ReadLine();
+
+            //7
+            Number7();
+
+            //8
+            Number8();
+
+        }
+
+        private static void Number7()
+        {
+            string[] names = { "Al Dente", "Anna Ghrahm", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+            for (int i = 0; i < names.Length; i++)
+            {
+                int k = i + 1;
+                Console.WriteLine("{0,2}. {1}", k, names[i]);
+            }
+            Console.ReadLine();
+        }
+
+        private static void Number8()
+        {
+            string[] names = { "Al Dente", "Anna Ghrahm", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
     }
 }
